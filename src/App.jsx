4681722fa7d5c97dd8ed981aa70bfd851dd09f22
Godspeexd12 <160627473+ypatel0101@users.noreply.github.com>
2024-04-07 20:03:55 -0500

    first push

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Authenticate from "./authenticate";
+import SignUpForm from "./signupform";
+export default function App() {
+  return (
+    <>
+      <Authenticate />
+      <SignUpForm />
+    </>
+  );
+}
---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+export default function SignUpForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+  try {
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  return (
+    <>
+      <h2>Sign Up</h2>
+      {error && <p>{error}</p>}
+            
+      <form onSubmit={handleSubmit}>
+                
+        <label>
+          Username:           
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />       
+        </label>
+                
+        <label>
+          Password:           
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />        
+        </label>
+        <button>Submit</button>
+              
+      </form>
+          
+    </>
+  );
+}
+
+
+const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup");
+const result = await response.json();
+console.log(result);
+
+
+
+export default function SignUpForm() {
+    return <h2>Sign Up!</h2>;
+  }
